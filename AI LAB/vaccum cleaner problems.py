cb9e2a90a2197a770a4b34bd432b9294eb15d026
suryasatvika03 <192524254.simats@saveetha.com>
2026-08-26 14:34:31 +0530

    Delete AI LAB directory

--- AI LAB/vaccum cleaner problems.py
+++
@@ -1,8 +0,0 @@
-rooms = {'A':'Dirty', 'B':'Dirty'}
-
-for room in rooms:
-    if rooms[room] == "Dirty":
-        print("Cleaning Room", room)
-        rooms[room] = "Clean"
-
-print("Final State:", rooms)
